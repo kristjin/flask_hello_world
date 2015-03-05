97b8eabf96f525a5aa2e489ceeaf94a46680c057
Matthew Conquergood <matthew.conquergood@gmail.com>
2015-03-05 06:28:26 +0000

    Hullo Heroku

--- hello_world.py
+++ hello_world.py
@@ -1,31 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 # static
 @app.route("/hello")
 def hello_world():
-    return "Hello World!"
+    return render_template('hello_world.html')
 
 # dynamic
 
 @app.route("/hello/<name>")
 def hello_person(name):
-    html = """
-        <h1>
-            Hello {}!
-        </h1>
-        <p>
-            Here's a picture of a kitten.  Awww...
-        </p>
-        <img src="http://placekitten.com/g/200/300">
-    """
-    return html.format(name.title())
+    return render_template('hello_template.html',
+                           name=name
+                          )
 
 @app.route("/jedi/<first>/<last>")
-def jedi_name(first, last): 
-    jediName = last[:3] + first[:2]
-    return "Hello, Jedi {}".format(jediName)
+def jedi_name(first, last):
+    return render_template('jedi_template.html',
+                           first=first,
+                           last=last)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
